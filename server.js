8a783504ab5141d77bb7d176ab7a2e4a191f9816
Karel Rudisar <krudisar@gmail.com>
2018-05-18 10:02:59 +0200

    reson for commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello!!! - Greetings from Vmware!!! - Come and see us at VMWorld 2018 ...\n');
+  response.end('==== |||||| ==== Hello!!! - Greetings from Vmware!!! - Come and see us at VMWorld 2018 ...\n');
 };
 
 var www = http.createServer(handleRequest);
